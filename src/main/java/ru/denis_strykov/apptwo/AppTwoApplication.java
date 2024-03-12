e4c2f00672c3fb95ec87df750494f6e2c2896cb5
Denis Strukov <d.strukov@astondevs.ru>
2024-03-13 00:09:38 +0300

    init commit

---
+++ src/main/java/ru/denis_strykov/apptwo/AppTwoApplication.java
@@ -0,0 +1,13 @@
+package ru.denis_strykov.apptwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppTwoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppTwoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/denis_strykov/apptwo/AppTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.denis_strykov.apptwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTwoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
